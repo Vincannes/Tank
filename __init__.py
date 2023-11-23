8ac41c2fc168be7a5ccf440f56967a9c3f05541b
vtrolard <60840864+Vincannes@users.noreply.github.com>
2023-11-23 22:38:21 +0100

    test

--- __init__.py
+++ __init__.py
@@ -2,6 +2,4 @@ import os
 import sys
 tank_dir = os.path.dirname(__file__)
 sys.path.append(tank_dir)
-sys.path.append(os.path.join(tank_dir, "yaml"))
-from python import yaml
 from python import sgtk
